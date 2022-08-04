5fc98a05593082dbb07d62a43cfc493c92ec1eb0
kirill.suptelov <kirill.suptelov@gmail.com>
2022-08-04 20:25:17 +0700

    Add LengthOfLastWord 	modified:   src/LengthOfLastWord/Solution.java

--- src/LengthOfLastWord/Solution.java
+++ src/LengthOfLastWord/Solution.java
@@ -1,4 +1,11 @@
 package LengthOfLastWord;
 
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("Hello World"));
+    }
+    public static int lengthOfLastWord(String s) {
+        var xx = s.trim().split("(\\W+)");
+        return xx[xx.length-1].length();
+    }
 }
