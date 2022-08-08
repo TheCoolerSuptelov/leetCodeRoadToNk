14ff4ee2315bcbbb47527ce25a3cf51fc36a1db1
kirill.suptelov <kirill.suptelov@gmail.com>
2022-08-08 19:25:43 +0700

    modified:   src/RemoveElement/Solution.java

--- src/RemoveElement/Solution.java
+++ src/RemoveElement/Solution.java
@@ -3,9 +3,21 @@ package RemoveElement;
 public class Solution {
     public int removeElement(int[] nums, int val) {
         int actualLength = 0;
-        for (int i = 0; i < nums.length; i++) {
-
+        int startIdx = 0;
+        int endIdx = 0;
+        while (endIdx < nums.length) {
+            int currValue = nums[endIdx];
+            if (currValue == val) {
+                while (endIdx < nums.length && nums[endIdx] == val) {
+                    endIdx++;
+                }
+                nums[startIdx++] = nums[endIdx];
+            }
+            actualLength++;
         }
-        return 0;
+        for (int i = actualLength; i <= nums.length; i++) {
+            nums[i] = 0;
+        }
+        return actualLength;
     }
 }
