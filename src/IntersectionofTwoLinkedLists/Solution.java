d429a259b0f6e3cc449dd89f2d34f976fd865e54
kirill.suptelov <kirill.suptelov@gmail.com>
2022-08-10 15:19:50 +0700

    IntersectionofTwoLinkedLists Solution 	modified:   src/IntersectionofTwoLinkedLists/Solution.java

--- src/IntersectionofTwoLinkedLists/Solution.java
+++ src/IntersectionofTwoLinkedLists/Solution.java
@@ -1,4 +1,31 @@
 package IntersectionofTwoLinkedLists;
 
+import java.util.HashMap;
+
 public class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int count = 0;
+        while(curA != curB){
+            if(count == 2) break;
+            if(curA == null) count++;
+            curA = curA == null ? headB : curA.next;
+            curB = curB == null ? headA : curB.next;
+        }
+        return (curA == curB) ? curA : null;
+    }
+
 }
