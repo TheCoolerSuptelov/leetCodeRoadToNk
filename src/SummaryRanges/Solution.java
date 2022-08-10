4d16fb40eed09348ba5b923cfed01849ed0cbb44
kirill.suptelov <kirill.suptelov@gmail.com>
2022-08-10 16:04:27 +0700

    Prettyfi SummaryRanges Solution 	modified:   src/SummaryRanges/Solution.java

--- src/SummaryRanges/Solution.java
+++ src/SummaryRanges/Solution.java
@@ -5,11 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 class Solution {
-    private static Object i;
-
     public static void main(String[] args) {
-        var testArr = new int[]{-1};
-        System.out.println("[-1]");
+        var testArr = new int[]{0,2,3,4,6,8,9};
+        System.out.println("[\"0\",\"2->4\",\"6\",\"8->9\"]");
         System.out.println(summaryRanges(testArr).stream().collect(Collectors.joining(",")));
     }
 
@@ -17,6 +15,7 @@ class Solution {
 
         var resultedList = new ArrayList<String>();
         if (nums == null || nums.length == 0) return resultedList;
+
         if (nums.length == 1) {
             resultedList.add(nums[0] + "");
             return resultedList;
@@ -27,30 +26,26 @@ class Solution {
         sb.append(nums[i]);
         int appendedValue = nums[i];
         for (; i < nums.length; i++) {
-
-
             if (i + 1 == nums.length) {
-                if (appendedValue != nums[i]){
+                if (appendedValue != nums[i]) {
                     sb.append("->");
                     sb.append(nums[i]);
                 }
                 resultedList.add(sb.toString());
-            }else if (nums[i] + 1 != nums[i + 1] && appendedValue == nums[i]){
-                resultedList.add(sb.toString());
-                sb.setLength(0);
-                appendedValue =nums[i + 1];
-                sb.append(appendedValue);
+                break;
+            }
+            if (nums[i] + 1 == nums[i + 1]) {
                 continue;
-            } else if (nums[i] + 1 != nums[i + 1]) {
-                sb.append("->");
-                sb.append(nums[i]);
-                resultedList.add(sb.toString());
-                sb.setLength(0);
-                appendedValue =nums[i + 1];
-                sb.append(appendedValue);
             }
 
-
+            if (appendedValue != nums[i]) {
+                sb.append("->");
+                sb.append(nums[i]);
+            }
+            resultedList.add(sb.toString());
+            sb.setLength(0);
+            appendedValue = nums[i + 1];
+            sb.append(appendedValue);
 
         }
         return resultedList;
