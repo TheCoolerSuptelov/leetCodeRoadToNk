da0509a1cdc9f75eb01a2ba7383caa02509a08e3
kirill.suptelov <kirill.suptelov@gmail.com>
2022-08-11 21:07:04 +0700

    ArrangingCoins Solution 	new file:   src/ArrangingCoins/Solution.java

---
+++ src/ArrangingCoins/Solution.java
@@ -0,0 +1,14 @@
+package ArrangingCoins;
+
+public class Solution {
+    public int arrangeCoins(int n) {
+        if (n == 1)return 1;
+        int totalRows = 0;
+        int sum = 0;
+        while(sum<=n){
+            sum += totalRows+1;
+            totalRows++;
+        }
+        return totalRows-1;
+    }
+}
