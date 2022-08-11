b9f9ef3cb043d745041f3e18337d31f464a00fe9
kirill.suptelov <kirill.suptelov@gmail.com>
2022-08-11 20:45:14 +0700

    Is PalindromeLinkedList Solution 	new file:   src/PalindromeLinkedList/Solution.java

---
+++ src/PalindromeLinkedList/Solution.java
@@ -0,0 +1,26 @@
+package PalindromeLinkedList;
+
+public class Solution {
+
+    public boolean isPalindrome(ListNode head) {
+
+        return false;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
