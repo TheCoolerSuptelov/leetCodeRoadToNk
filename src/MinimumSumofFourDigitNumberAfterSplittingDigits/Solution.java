0dc5feab28f5feccaf7c20452c34c13d579f27f8
kirill.suptelov <kirill.suptelov@gmail.com>
2022-08-14 17:38:58 +0700

    Add MinimumSumofFourDigitNumberAfterSplittingDigits Solution 	modified:   src/MinimumSumofFourDigitNumberAfterSplittingDigits/Solution.java

--- src/MinimumSumofFourDigitNumberAfterSplittingDigits/Solution.java
+++ src/MinimumSumofFourDigitNumberAfterSplittingDigits/Solution.java
@@ -1,4 +1,19 @@
 package MinimumSumofFourDigitNumberAfterSplittingDigits;
 
-public class Solution {
+import java.util.Arrays;
+
+/**
+ *
+ * Input: num = 2932
+ * Output: 52
+ * Explanation: Some possible pairs [new1, new2] are [29, 23], [223, 9], etc.
+ * The minimum sum can be obtained by the pair [29, 23]: 29 + 23 = 52.
+ *
+ * */
+class Solution {
+    public int minimumSum(int num) {
+        char[] ch = (num+"").toCharArray();
+        Arrays.sort(ch);
+        return Integer.parseInt(""+ch[0] +ch[2]) + Integer.parseInt(""+ch[1] +ch[3]);
+    }
 }
