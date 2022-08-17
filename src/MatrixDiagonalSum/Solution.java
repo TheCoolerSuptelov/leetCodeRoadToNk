dacebe96b4e8c031c56ee23da73204a287ae6ecf
Kirill Suptelov <kirill.suptelov@gmail.com>
2022-08-17 22:21:53 +0700

    Add MatrixDiagonalSum 	modified:   src/MatrixDiagonalSum/Solution.java

--- src/MatrixDiagonalSum/Solution.java
+++ src/MatrixDiagonalSum/Solution.java
@@ -1,14 +1,13 @@
 package MatrixDiagonalSum;
+
 /**
- *
  * Input: mat = [[1,2,3],
-                 * [4,5,6],
-                 * [7,8,9]]
+ * [4,5,6],
+ * [7,8,9]]
  * Output: 25
  * Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
  * Notice that element mat[1][1] = 5 is counted only once.
- *
- * */
+ */
         /*
         00 11 22
         02 11 20
@@ -21,8 +20,9 @@ package MatrixDiagonalSum;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(diagonalSum(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
+        System.out.println(diagonalSum(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
     }
+
     public static int diagonalSum(int[][] mat) {
         int sum = 0;
 
@@ -30,8 +30,8 @@ public class Solution {
             sum += mat[i][i];
         }
         for (int i = 0; i < mat.length; i++) {
-            if (i == (mat.length-1-i)) continue;
-            sum += mat[i][mat.length-1-i];
+            if (i == (mat.length - 1 - i)) continue;
+            sum += mat[i][mat.length - 1 - i];
         }
 
         return sum;
