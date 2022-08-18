d871839cf807420ba23294ef1bc02b975bcb8bc8
Kirill Suptelov <kirill.suptelov@gmail.com>
2022-08-18 23:22:25 +0700

    Add Solution 	new file:   src/FlippinganImage/Solution.java

---
+++ src/FlippinganImage/Solution.java
@@ -0,0 +1,20 @@
+package FlippinganImage;
+/**
+ *
+ * Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+ * Output: [[1,0,0],[0,1,0],[1,1,1]]
+ * Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+ * Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+ *
+ * */
+public class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[i].length; j++) {
+                if (image[i][j]==0)image[i][j]=1;
+                if (image[i][j]==1)image[i][j]=0;
+            }
+        }
+        return image;
+    }
+}
