855ff417c03e4dac598b8b41572738aa3f8351ef
Kirill Suptelov <kirill.suptelov@gmail.com>
2022-08-22 20:11:03 +0700

    ADd EncodeandDecodeTinyURL solution 	modified:   .idea/workspace.xml 	new file:   src/EncodeandDecodeTinyURL/Codec.java

---
+++ src/EncodeandDecodeTinyURL/Codec.java
@@ -0,0 +1,23 @@
+package EncodeandDecodeTinyURL;
+
+import java.util.HashMap;
+
+public class Codec {
+    public static final String domenURL = "http://tinyurl.com/";
+    public HashMap<String,String> storage = new HashMap<>();
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        String shortUrl = domenURL + longUrl.hashCode();
+        storage.put(shortUrl, longUrl);
+        return shortUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return storage.get(shortUrl);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));1`
